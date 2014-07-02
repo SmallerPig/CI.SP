263779fbccdd8b9d2bdb6d5eff9d5dbe2677ef68
SmallerPig <zhuqiyancheng@126.com>
2014-07-02 22:47:18 +0800

    init the project of IC.SP

---
+++ application/views/Home/index.php
@@ -0,0 +1,100 @@
+
+    <div class="contentRow">
+        <div id="pictureSearchArea">
+            <div id="pictureSearchAreaTitle">图片搜索</div>
+            <div id="pictureSearchAreaContentWrapper">
+                <input id="pictureSearchInput" onkeydown="if (event.keyCode == 13) { doSubmit() }" class="inputText" placeholder="输入关键词或图片编号" />
+                <a id="pictureSearchSubmit" href="javascript:"></a>
+                <a id="pictureSearchLink" href="/Pics/Search">高级搜索</a>
+            </div>
+        </div>
+    </div>
+    <div class="contentRow">
+        <!-- banner开始 -->
+        <div id="slidePicArea">
+            <div id="slidePicWrapper">
+                <a href="javascript:void(0)">
+                    <img class="slidePicItem" src="/srctemp/QQ截图20140512094825.png" />
+                </a>
+                <a href="javascript:void(0)">
+                    <img class="slidePicItem" src="/srctemp/QQ截图20140512094825.png" />
+                </a>
+                <a href="javascript:void(0)">
+                    <img class="slidePicItem" src="/srctemp/QQ截图20140512094825.png" />
+                </a>
+                <a href="javascript:void(0)">
+                    <img class="slidePicItem" src="/srctemp/QQ截图20140512094825.png" />
+                </a>
+                <a href="javascript:void(0)">
+                    <img class="slidePicItem" src="/srctemp/QQ截图20140512094825.png" />
+                </a>
+            </div>
+            <div id="slidePicInfoArea">
+                <!--<div class="slidePicItemMark"></div>-->
+            </div>
+        </div>
+        <!-- banner结束 -->
+        <!-- 新闻与活动开始 -->
+        <div id="newsArea">
+            <div class="titleArea">
+                <div class="titleText">新闻与活动</div>
+                <a class="moreLink" href="javascript:void(0)">更多&gt;</a>
+            </div>
+            <div class="contentArea">
+                <a href="javascript:void(0)" class="newsTitle">新闻信息标题新闻信息标题新闻信息标题新闻信息标题</a>
+                <a href="javascript:void(0)" class="newsTitle">新闻信息标题新闻信息标题新闻信息标题新闻信息标题</a>
+                <a href="javascript:void(0)javascript:void(0)" class="newsTitle">新闻信息标题新闻信息标题新闻信息标题新闻信息标题</a>
+                <a href="javascript:void(0)" class="newsTitle">新闻信息标题新闻信息标题新闻信息标题新闻信息标题</a>
+                <a href="javascript:void(0)" class="newsTitle">新闻信息标题新闻信息标题新闻信息标题新闻信息标题</a>
+                <a href="javascript:void(0)" class="newsTitle">新闻信息标题新闻信息标题新闻信息标题新闻信息标题</a>
+                <a href="javascript:void(0)" class="newsTitle">新闻信息标题新闻信息标题新闻信息标题新闻信息标题</a>
+                <a href="javascript:void(0)" class="newsTitle">新闻信息标题新闻信息标题新闻信息标题新闻信息标题</a>
+            </div>
+        </div>
+        <!-- 新闻与活动结束 -->
+    </div>
+
+<!-- 专题开始 -->
+    <div class="contentRow">
+        <div id="topicArea">
+            <div class="titleArea">
+                <div class="titleText">专题</div>
+                <a class="moreLink" href="javascript:void(0)javascript:void(0)">更多&gt;</a>
+            </div>
+            <div class="contentArea">
+                <a class="topicItem" href="javascript:void(0)javascript:void(0)">
+                    <img class="topicImage" src="/srctemp/QQ截图20140512094825.png" />
+                    <div class="textTitle">专题标题专题标题专题标题专题标题专题标题专题标题专题标题专题标题专题标题专题标题</div>
+                    <div class="textBrief">专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息</div>
+                </a>
+                <a class="topicItem" href="javascript:void(0)javascript:void(0)">
+                    <img class="topicImage" src="/srctemp/QQ截图20140512094825.png" />
+                    <div class="textTitle">专题标题专题标题专题标题专题标题专题标题专题标题专题标题专题标题专题标题专题标题</div>
+                    <div class="textBrief">专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息</div>
+                </a>
+                <a class="topicItem" href="javascript:void(0)javascript:void(0)">
+                    <img class="topicImage" src="/srctemp/QQ截图20140512094825.png" />
+                    <div class="textTitle">专题标题专题标题专题标题专题标题专题标题专题标题专题标题专题标题专题标题专题标题</div>
+                    <div class="textBrief">专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息</div>
+                </a>
+                <a class="topicItem" href="javascript:void(0)javascript:void(0)">
+                    <img class="topicImage" src="/srctemp/QQ截图20140512094825.png" />
+                    <div class="textTitle">专题标题专题标题专题标题专题标题专题标题专题标题专题标题专题标题专题标题专题标题</div>
+                    <div class="textBrief">专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息专题信息</div>
+                </a>
+            </div>
+        </div>
+        <div id="rightSideAdArea">
+            <a class="adWrapper" href="javascript:void(0)"><img class="adImage" src="/srctemp/QQ截图20140512094825.png"></a>
+            <a class="adWrapper" href="javascript:void(0)"><img class="adImage" src="/srctemp/QQ截图20140512094825.png"></a>
+        </div>
+    </div>
+    <!-- 专题结束 -->
+
+
+    <link rel="stylesheet" type="text/css" href="/content/src/css/home.css">
+
+    <script src="/content/src/js/home.js"></script>
+    <script src="/Scripts/jquery-1.8.2.min.js"></script>
+
+
